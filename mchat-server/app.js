642a3131e2b28f66ffa7919064eb0e3660cfd7cd
ankur598 <ankursingh.as278@gmail.com>
2024-02-13 21:53:06 +0530

    added live chat for two person

--- mchat-server/app.js
+++
@@ -1,9 +0,0 @@
-//start writing backend
-let express = require("express");
-let app = express();
-
-app.use(express.json());
-
-app.listen(1998, () => {
-  console.log("app is listening on 1998");
-});
